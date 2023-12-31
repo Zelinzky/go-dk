1edbf8d76afb6987f87cf50261b318f67b608c44
Roberto L. Taborda <robert@zelinzky.dev>
2023-12-30 22:36:03 -0500

    Create server handling and setup for new Go project
    The commit contains the foundational setup for a Go project, including the creation of a server and route handler infrastructure. It also incorporates the addition of unit and integration tests functionality. More specifically, the changes establish server listening, error handling capability, and a server cleanup function for testing purposes.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-dk
 
 go 1.21.5
+
+require (
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/matryer/is v1.4.1 // indirect
+)
---
+++ integrationtest/server.go
@@ -0,0 +1,48 @@
+package integrationtest
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"go-dk/server"
+)
+
+// CreateServer for testing on port 8081, returning a cleanup function to stop the server
+// Usage:
+//
+//	cleanup := CreateServer()
+//	defer cleanup()
+func CreateServer() func() {
+	s := server.New(server.Options{
+		Host: "localhost",
+		Port: 8081,
+	})
+
+	go func() {
+		if err := s.Start(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for {
+		_, err := http.Get("http://localhost:8081")
+		if err == nil {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	return func() {
+		if err := s.Stop(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// SkipIfShort skips t if the "-short" flag is passed to "go test".
+func SkipIfShort(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+}
